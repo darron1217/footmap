1a45ce89dcf4a65a69a1d192d683841b39ac66f1
Darron Park <darron1217@gmail.com>
2019-10-22 19:48:37 +0900

    로그인 Prompt & 관리자페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,73 @@
     <ion-header>
       <ion-toolbar color="warning">
         <ion-title>푸트맵</ion-title>
+        <ion-buttons slot="secondary">
+          <ion-button @click="showLoginPrompt">
+            <ion-icon slot="icon-only" name="person"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    openLogin() {
+      return this.$ionic.modalController
+        .create({
+          component: Modal,
+          componentProps: {
+            data: {
+              content: 'New Content',
+            },
+            propsData: {
+              title: 'New title',
+            },
+          },
+        })
+        .then(m => m.present())
+    },
+    // 로그인
+    showLoginPrompt() {
+      return this.$ionic.alertController
+        .create({
+          header: '관리자 확인',
+          inputs: [
+            {
+              name: 'password',
+              type: 'password',
+              placeholder: '비밀번호를 입력하세요',
+            },
+          ],
+          buttons: [
+            {
+              text: '취소',
+              role: 'cancel',
+              cssClass: 'secondary',
+              handler: () => {
+                console.log('Confirm Cancel')
+              },
+            },
+            {
+              text: '확인',
+              handler: () => {
+                console.log('Confirm Ok');
+                // 관리자페이지로 이동
+                this.$router.push('/admin');
+              },
+            },
+          ],
+        })
+        .then(a => a.present());
+    },
+  },
+}
+
+
+</script>
 
 <style>
 /* Core CSS required for Ionic components to work properly */
@@ -30,10 +92,6 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  
 }
 </style>
---
+++ src/views/Admin.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="admin">
+    <h1>관리자 페이지</h1>
+  </div>
+</template>
